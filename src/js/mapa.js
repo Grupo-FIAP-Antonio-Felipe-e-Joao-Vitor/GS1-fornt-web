036ebfeb7d3d361eb82be6d3162d9fc8f5f2d7a9
Antonio <antoniojacintoa@gmail.com>
2025-05-28 18:51:35 -0300

    criando o home

---
+++ src/js/mapa.js
@@ -0,0 +1,6 @@
+let map = L.map('mapa').setView([-23.5505, -46.6333], 10);
+
+//CONTRUINDO A APLICAÇÃO DO MAP DA API
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+}).addTo(map);
