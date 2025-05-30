4ab6cb72e8c10f77dfb2eaf189e65493766c1b4e
Antonio <antoniojacintoa@gmail.com>
2025-05-30 08:48:25 -0300

    fazendo funcionalidade mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -4,3 +4,8 @@ let map = L.map('mapa').setView([-23.5505, -46.6333], 10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+function pegarCEP () {
+    campoCEP = document.getElementById("pesquisa-CEP")
+    campoCEP.mask("00000-000")
+}
